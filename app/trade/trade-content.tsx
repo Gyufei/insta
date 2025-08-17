aa7d31fdf284a95e6db7e17e22ecef45c0624bab
fei guo <812773673@qq.com>
2025-08-18 01:28:55 +0800

    fix:error-52

--- app/trade/trade-content.tsx
+++ app/trade/trade-content.tsx
@@ -1,44 +1,43 @@
 'use client';
 
-// import { CircleX, Loader } from 'lucide-react';
-// import { divide } from 'safebase';
-// import { useEffect, useState } from 'react';
-import { useState } from 'react';
+import { CircleX, Loader } from 'lucide-react';
+import { divide } from 'safebase';
+
+import { useEffect, useState } from 'react';
 
 import Image from 'next/image';
 
-// import {
-//   DEFAULT_NATIVE_ADDRESS,
-//   DEFAULT_TOKEN_DECIMALS,
-//   replaceNativeAddressUseBackend,
-// } from '@/config/network-config';
+import {
+  DEFAULT_NATIVE_ADDRESS,
+  DEFAULT_TOKEN_DECIMALS,
+  replaceNativeAddressUseBackend,
+} from '@/config/network-config';
 import { IToken } from '@/config/tokens';
 
 // import { TokenDropSelector } from '@/components/common/token-drop-selector';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 
-// import { useUniswapQuote } from '@/lib/data/use-uniswap-quote';
-// import { useUniswapSwap } from '@/lib/data/use-uniswap-swap';
-// import { ErrorVO } from '@/lib/model/error-vo';
-// import { eventBus } from '@/lib/state/eventBus';
+import { useUniswapQuote } from '@/lib/data/use-uniswap-quote';
+import { useUniswapSwap } from '@/lib/data/use-uniswap-swap';
+import { ErrorVO } from '@/lib/model/error-vo';
+import { eventBus } from '@/lib/state/eventBus';
 import { cn } from '@/lib/utils';
-
 // import { useGetAccountBalance } from '@/lib/web3/use-get-account-balance';
 
-// import { SlippageSettings } from '../(protocols)/uniswap/swap/slippage-settings';
+import { SlippageSettings } from '../(protocols)/uniswap/swap/slippage-settings';
 
 export function TokenContent() {
   const [sellToken, setSellToken] = useState<IToken | undefined>(undefined);
   const [buyToken, setBuyToken] = useState<IToken | undefined>(undefined);
   const [sellValue, setSellValue] = useState('');
   const [buyValue, setBuyValue] = useState('');
-  // const [slippage, setSlippage] = useState('1');
+  const [slippage, setSlippage] = useState('1');
 
-  // const [errorData, setErrorData] = useState<ErrorVO>({
-  //   showError: false,
-  //   errorMessage: '',
-  // });
+  const [errorData, setErrorData] = useState<ErrorVO>({
+    showError: false,
+    errorMessage: '',
+  });
 
   const [rotateTimes, setRotateTimes] = useState(0);
 
@@ -52,104 +51,104 @@ export function TokenContent() {
   //   true
   // );
 
-  // const quoteParams =
-  //   sellToken && buyToken && sellValue
-  //     ? {
-  //         tokenIn: replaceNativeAddressUseBackend(sellToken.address),
-  //         tokenOut: replaceNativeAddressUseBackend(buyToken.address),
-  //         amountIn: sellValue,
-  //         amountInDecimals: sellToken.decimals?.toString() || DEFAULT_TOKEN_DECIMALS.toString(),
-  //       }
-  //     : undefined;
+  const quoteParams =
+    sellToken && buyToken && sellValue
+      ? {
+          tokenIn: replaceNativeAddressUseBackend(sellToken.address),
+          tokenOut: replaceNativeAddressUseBackend(buyToken.address),
+          amountIn: sellValue,
+          amountInDecimals: sellToken.decimals?.toString() || DEFAULT_TOKEN_DECIMALS.toString(),
+        }
+      : undefined;
 
-  // const {
-  //   data: quoteData,
-  //   isLoading: isQuoteLoading,
-  //   error: quoteError,
-  // } = useUniswapQuote(quoteParams);
+  const {
+    data: quoteData,
+    isLoading: isQuoteLoading,
+    error: quoteError,
+  } = useUniswapQuote(quoteParams);
 
-  // const { mutate: swap, isPending: isSwapPending, error: swapError } = useUniswapSwap();
+  const { mutate: swap, isPending: isSwapPending, error: swapError } = useUniswapSwap();
 
-  // useEffect(() => {
-  //   if (quoteData?.output) {
-  //     setBuyValue(
-  //       divide(quoteData.output, String(10 ** (buyToken?.decimals || DEFAULT_TOKEN_DECIMALS)))
-  //     );
-  //   }
-  // }, [quoteData?.output, buyToken?.decimals]);
+  useEffect(() => {
+    if (quoteData?.output) {
+      setBuyValue(
+        divide(quoteData.output, String(10 ** (buyToken?.decimals || DEFAULT_TOKEN_DECIMALS)))
+      );
+    }
+  }, [quoteData?.output, buyToken?.decimals]);
 
-  // useEffect(() => {
-  //   if (quoteError) {
-  //     let errorMsg = quoteError.message;
-  //     if (quoteError.message.includes(`Cannot read properties of undefined (reading 'quote')`)) {
-  //       errorMsg = 'Insufficient liquidity, please try again later';
-  //     }
-  //     setErrorData({
-  //       showError: true,
-  //       errorMessage: errorMsg,
-  //     });
-  //   }
+  useEffect(() => {
+    if (quoteError) {
+      let errorMsg = quoteError.message;
+      if (quoteError.message.includes(`Cannot read properties of undefined (reading 'quote')`)) {
+        errorMsg = 'Insufficient liquidity, please try again later';
+      }
+      setErrorData({
+        showError: true,
+        errorMessage: errorMsg,
+      });
+    }
 
-  //   if (swapError) {
-  //     setErrorData({
-  //       showError: true,
-  //       errorMessage: swapError.message,
-  //     });
-  //   }
+    if (swapError) {
+      setErrorData({
+        showError: true,
+        errorMessage: swapError.message,
+      });
+    }
 
-  //   if (!swapError && !quoteError) {
-  //     setErrorData({
-  //       showError: false,
-  //       errorMessage: '',
-  //     });
-  //   }
-  // }, [swapError, quoteError]);
+    if (!swapError && !quoteError) {
+      setErrorData({
+        showError: false,
+        errorMessage: '',
+      });
+    }
+  }, [swapError, quoteError]);
 
-  // const [init, setInit] = useState(false);
-  // useEffect(() => {
-  //   if (!init) {
-  //     const token = sessionStorage.getItem('token');
-  //     if (token) {
-  //       setSellToken(JSON.parse(token));
-  //       sessionStorage.removeItem('token');
-  //     }
-  //     setInit(true);
-  //   }
-  // }, [init]);
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    if (!init) {
+      const token = sessionStorage.getItem('token');
+      if (token) {
+        setSellToken(JSON.parse(token));
+        sessionStorage.removeItem('token');
+      }
+      setInit(true);
+    }
+  }, [init]);
 
-  // useEffect(() => {
-  //   const unsubscribe = eventBus.subscribe(
-  //     'trade-token',
-  //     (data: { name: string; props: { token: IToken } }) => {
-  //       console.log('data', data);
-  //       if (data.name === 'TradeToken') {
-  //         setSellToken(data.props.token);
-  //         setInit(true);
-  //       }
-  //     }
-  //   );
-  //   return () => unsubscribe();
-  // }, []);
+  useEffect(() => {
+    const unsubscribe = eventBus.subscribe(
+      'trade-token',
+      (data: { name: string; props: { token: IToken } }) => {
+        console.log('data', data);
+        if (data.name === 'TradeToken') {
+          setSellToken(data.props.token);
+          setInit(true);
+        }
+      }
+    );
+    return () => unsubscribe();
+  }, []);
 
-  // function handleSwap() {
-  //   if (!quoteData || !sellToken || !buyToken) return;
+  function handleSwap() {
+    if (!quoteData || !sellToken || !buyToken) return;
 
-  //   const isSellTokenEth = sellToken.address === DEFAULT_NATIVE_ADDRESS;
-  //   const isBuyTokenEth = buyToken.address === DEFAULT_NATIVE_ADDRESS;
+    const isSellTokenEth = sellToken.address === DEFAULT_NATIVE_ADDRESS;
+    const isBuyTokenEth = buyToken.address === DEFAULT_NATIVE_ADDRESS;
 
-  //   // 清除之前的错误
-  //   setErrorData({
-  //     showError: false,
-  //     errorMessage: '',
-  //   });
+    // 清除之前的错误
+    setErrorData({
+      showError: false,
+      errorMessage: '',
+    });
 
-  //   swap({
-  //     token_in_is_eth: isSellTokenEth,
-  //     token_out_is_eth: isBuyTokenEth,
-  //     slippage: (Number(slippage) * 1e16).toString(),
-  //     route: quoteData.route[0],
-  //   });
-  // }
+    swap({
+      token_in_is_eth: isSellTokenEth,
+      token_out_is_eth: isBuyTokenEth,
+      slippage: (Number(slippage) * 1e16).toString(),
+      route: quoteData.route[0],
+    });
+  }
 
   const handleSwapTokens = () => {
     setRotateTimes((rotateTimes % 2) + 1);
@@ -162,16 +161,16 @@ export function TokenContent() {
     setSellValue(buyValue);
     setBuyValue(tempValue);
 
-    // 清除错误
-    // setErrorData({
-    //   showError: false,
-    //   errorMessage: '',
-    // });
+    // 清除错误;
+    setErrorData({
+      showError: false,
+      errorMessage: '',
+    });
   };
 
   // const handleMaxClick = () => {
   //   if (sellToken) {
-  //     // setSellValue(fromBalance);
+  //     setSellValue(fromBalance);
   //   }
   // };
 
@@ -231,10 +230,10 @@ export function TokenContent() {
         </div>
 
         <div className="flex md:flex-row flex-col md:justify-between md:items-center mt-5 gap-2 md:gap-0">
-          {/* <SlippageSettings onSlippageChange={setSlippage} /> */}
+          <SlippageSettings onSlippageChange={setSlippage} />
 
           <div className="flex flex-col md:items-center md:flex-row gap-2 md:gap-1 justify-end">
-            {/* {errorData.showError && (
+            {errorData.showError && (
               <div className={cn('rounded-sm bg-red-400/15 dark:bg-red-500/10 p-2 mt-0')}>
                 <div className="flex">
                   <div className="flex-shrink-0">
@@ -249,12 +248,12 @@ export function TokenContent() {
                   </div>
                 </div>
               </div>
-            )} */}
+            )}
             <Button
               className="min-w-40 h-12 text-xl font-medium flex leading-[24px] items-center justify-center rounded-md bg-[#6E75F9] text-white hover:bg-[#6E75F990]"
-              // onClick={handleSwap}
+              onClick={handleSwap}
             >
-              {/* {isQuoteLoading ? (
+              {isQuoteLoading ? (
                 <span className="flex items-center gap-1">
                   <Loader className="w-6 h-6 animate-spin" />
                   <span>Finalizing quote</span>
@@ -264,7 +263,7 @@ export function TokenContent() {
                   {isSwapPending && <Loader className="w-6 h-6 mr-1 animate-spin" />}
                   <span>Swap</span>
                 </span>
-              )} */}
+              )}
             </Button>
           </div>
         </div>
