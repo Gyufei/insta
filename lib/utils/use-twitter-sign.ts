a6e9d8a6b7765085196a7da25ce19201b48cf24e
fei guo <812773673@qq.com>
2025-08-18 18:58:11 +0800

    feat:twitter-key

--- lib/utils/use-twitter-sign.ts
+++ lib/utils/use-twitter-sign.ts
@@ -13,7 +13,7 @@ export function useTwitterSign() {
 
   function goTwitter(cb: string) {
     window.location.href = isProduction
-      ? `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=NlF6aWE5Yk9kU1hfQUl2bkhLX1Y6MTpjaQ&redirect_uri=${cb}&scope=users.read%20tweet.read%20offline.access%20space.read&state=state&code_challenge=challenge&code_challenge_method=plain`
+      ? `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=NWxiS1k1WnpIVFpGdFg2YmtfQk46MTpjaQ&redirect_uri=${cb}&scope=users.read%20tweet.read%20offline.access%20space.read&state=state&code_challenge=challenge&code_challenge_method=plain`
       : `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=NlF6aWE5Yk9kU1hfQUl2bkhLX1Y6MTpjaQ&redirect_uri=${cb}&scope=users.read%20tweet.read%20offline.access%20space.read&state=state&code_challenge=challenge&code_challenge_method=plain`;
   }
 
