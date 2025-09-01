53a372ff17c77fe5c4329b894af6672bb741dcfd
fei guo <812773673@qq.com>
2025-09-01 19:09:42 +0800

    feat:new-user-chanllenge-random

--- lib/utils/use-twitter-sign.ts
+++ lib/utils/use-twitter-sign.ts
@@ -2,6 +2,15 @@ import { useSearchParams } from 'next/navigation';
 
 import { isProduction } from '../data/api-path';
 
+function generateRandomString(length: number) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
 export function useTwitterSign() {
   const searchParams = useSearchParams();
 
@@ -12,9 +21,10 @@ export function useTwitterSign() {
   const error = searchParams.get('error') || from?.includes('error');
 
   function goTwitter(cb: string) {
+    const codeChange = generateRandomString(6);
     window.location.href = isProduction
-      ? `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=NWxiS1k1WnpIVFpGdFg2YmtfQk46MTpjaQ&redirect_uri=${cb}&scope=users.read%20tweet.read%20offline.access%20space.read&state=state&code_challenge=challenge&code_challenge_method=plain`
-      : `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=NlF6aWE5Yk9kU1hfQUl2bkhLX1Y6MTpjaQ&redirect_uri=${cb}&scope=users.read%20tweet.read%20offline.access%20space.read&state=state&code_challenge=challenge&code_challenge_method=plain`;
+      ? `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=NWxiS1k1WnpIVFpGdFg2YmtfQk46MTpjaQ&redirect_uri=${cb}&scope=users.read%20tweet.read%20offline.access%20space.read&state=state&code_challenge=${codeChange}&code_challenge_method=plain`
+      : `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=NlF6aWE5Yk9kU1hfQUl2bkhLX1Y6MTpjaQ&redirect_uri=${cb}&scope=users.read%20tweet.read%20offline.access%20space.read&state=state&code_challenge=${codeChange}&code_challenge_method=plain`;
   }
 
   function removeXVerifyCode() {
