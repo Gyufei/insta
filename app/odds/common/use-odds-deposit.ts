ba7ac38458a6804966d85caf05a73c70873caecc
fei guo <812773673@qq.com>
2025-09-12 10:43:19 +0800

    fix:eoa-transfer-error

--- app/odds/common/use-odds-deposit.ts
+++ app/odds/common/use-odds-deposit.ts
@@ -2,14 +2,16 @@ import { ERROR_MESSAGES, SUCCESS_MESSAGES } from '@/config/const-msg';
 
 import { ApiPath } from '@/lib/data/api-path';
 import { createMutationHook } from '@/lib/data/helpers';
+import { useAccountStore } from '@/lib/state/account';
 
 import { useOddsUserInfo } from './use-user-info';
 
 export interface IOddsDepositParams {
   wallet: string;
-  sandbox_account: string;
+  sandbox_account?: string;
   odds_user_id: string;
   amount: string;
+  wallet_type: string;
   [key: string]: string | undefined;
 }
 
@@ -20,17 +22,30 @@ export interface IOddsDepositArgs {
 export function useOddsDeposit() {
   const { data: userInfo } = useOddsUserInfo();
   const userId = userInfo?.user_id;
+  const { currentAccountType } = useAccountStore();
 
   return createMutationHook<IOddsDepositParams>(
     ApiPath.oddsDeposit,
     (args: unknown, address: string, account: string) => {
       const params = args as IOddsDepositParams;
-      return {
-        wallet: address,
-        sandbox_account: account,
-        odds_user_id: userId || '',
-        amount: params.amount,
-      };
+
+      const reqArgs =
+        currentAccountType === 'EOA'
+          ? {
+              wallet: address,
+              odds_user_id: userId || '',
+              amount: params.amount,
+              wallet_type: currentAccountType,
+            }
+          : {
+              wallet: address,
+              sandbox_account: account,
+              odds_user_id: userId || '',
+              amount: params.amount,
+              wallet_type: currentAccountType,
+            };
+
+      return reqArgs;
     },
     SUCCESS_MESSAGES.TRANSFER_TO_TRADING_SUCCESS,
     ERROR_MESSAGES.TRANSFER_TO_TRADING_FAILED,
--- app/odds/common/use-odds-withdraw.ts
+++ app/odds/common/use-odds-withdraw.ts
@@ -2,14 +2,16 @@ import { ERROR_MESSAGES, SUCCESS_MESSAGES } from '@/config/const-msg';
 
 import { ApiPath } from '@/lib/data/api-path';
 import { createMutationHook } from '@/lib/data/helpers';
+import { useAccountStore } from '@/lib/state/account';
 
 import { useOddsUserInfo } from './use-user-info';
 
 export interface IOddsWithdrawParams {
   wallet: string;
-  sandbox_account: string;
+  sandbox_account?: string;
   odds_user_id: string;
   amount: string;
+  wallet_type: string;
   [key: string]: string | undefined;
 }
 
@@ -19,18 +21,31 @@ export interface IOddsWithdrawArgs {
 
 export function useOddsWithdraw() {
   const { data: userInfo } = useOddsUserInfo();
+  const { currentAccountType } = useAccountStore();
   const userId = userInfo?.user_id;
 
   return createMutationHook<IOddsWithdrawParams>(
     ApiPath.oddsWithdraw,
     (args: unknown, address: string, account: string) => {
       const params = args as IOddsWithdrawArgs;
-      return {
-        wallet: address,
-        sandbox_account: account,
-        odds_user_id: userId || '',
-        amount: params.amount,
-      };
+
+      const reqArgs =
+        currentAccountType === 'EOA'
+          ? {
+              wallet: address,
+              odds_user_id: userId || '',
+              amount: params.amount,
+              wallet_type: currentAccountType,
+            }
+          : {
+              wallet: address,
+              sandbox_account: account,
+              odds_user_id: userId || '',
+              amount: params.amount,
+              wallet_type: currentAccountType,
+            };
+
+      return reqArgs;
     },
     SUCCESS_MESSAGES.TRANSFER_TO_FUNDING_SUCCESS,
     ERROR_MESSAGES.TRANSFER_TO_FUNDING_FAILED,
