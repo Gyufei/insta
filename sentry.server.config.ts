dd638009fc0679734e533903f97795660c54bb8a
fei guo <812773673@qq.com>
2025-09-15 17:38:45 +0800

    feat:no-sentry-in-dev

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,6 +6,9 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: 'https://d76f76cf9282d4721587388fba6c0840@o4508368229498880.ingest.de.sentry.io/4508368267378768',
 
+  // Disable Sentry reporting in local development
+  enabled: process.env.NODE_ENV === 'production',
+
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
