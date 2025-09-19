ae5ebecd1008cb89bcbc3b9b9f71916504431545
fei guo <812773673@qq.com>
2025-09-19 14:37:22 +0800

    feat:account-setting

--- components/side-drawer/account-setting/account-display/index.tsx
+++ components/side-drawer/account-setting/account-display/index.tsx
@@ -1,12 +1,20 @@
+import { useAccountStore } from '@/lib/state/account';
+
 import { AccountList } from './account-list';
 import { AuthorityManage } from './authority-manage';
 
 export function AccountDisplay() {
+  const { currentAccountType } = useAccountStore();
+
   return (
     <>
       <div className="text-base font-semibold mb-4 mt-8">DSA</div>
       <AccountList />
-      <AuthorityManage />
+      {currentAccountType === 'DSA' && (
+        <>
+          <AuthorityManage />
+        </>
+      )}
     </>
   );
 }
