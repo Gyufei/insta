f5cc60ce0ec87179302c04803ccc04ebbc2ecf46
fei guo <812773673@qq.com>
2025-09-21 13:11:51 +0800

    fix:twitter-link

--- lib/utils/use-twitter-sign.ts
+++ lib/utils/use-twitter-sign.ts
@@ -11,9 +11,11 @@ export function useTwitterSign() {
   const error = searchParams.get('error');
 
   function goTwitter(cb: string) {
-    window.location.href = isProduction
+    const href = isProduction
       ? `https://x.com/i/oauth2/authorize?response_type=code&client_id=NWxiS1k1WnpIVFpGdFg2YmtfQk46MTpjaQ&redirect_uri=${cb}&scope=users.read%20tweet.read%20offline.access&state=state&code_challenge=challenge&code_challenge_method=plain`
       : `https://x.com/i/oauth2/authorize?response_type=code&client_id=NlF6aWE5Yk9kU1hfQUl2bkhLX1Y6MTpjaQ&redirect_uri=${cb}&scope=users.read%20tweet.read%20offline.access&state=state&code_challenge=challenge&code_challenge_method=plain`;
+      
+    window.open(href, '_blank');
   }
 
   function removeXVerifyCode() {
