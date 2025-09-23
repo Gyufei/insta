1d17d0cb9ee003a79a6b4bc4972d221e7bebf818
fei guo <812773673@qq.com>
2025-09-23 18:29:58 +0800

    fix:error-style

--- app/odds/components/MarketTitle.tsx
+++ app/odds/components/MarketTitle.tsx
@@ -29,19 +29,19 @@ export default function MarketTitle({ title, id }: MarketTitleProps) {
       className="h-fit w-full relative cursor-pointer group block"
       href={`/odds/market/${id}?chain=monad`}
     >
-      <div className="relative h-[40px] mb-2">
+      <div className="relative h-[24px] mb-2">
         <div
           className={
             'w-full' +
             (isOverflowing
-              ? ' group-hover:absolute group-hover:top-0 group-hover:inset-x-0 group-hover:z-20 group-hover:border group-hover:shadow-lg group-hover:rounded-md group-hover:bg-background group-hover:pt-1 group-hover:pb-2 group-hover:px-2'
+              ? ' group-hover:absolute group-hover:top-0 group-hover:inset-x-0 group-hover:z-20 group-hover:rounded-md group-hover:bg-white group-hover:pt-1 group-hover:pb-2 group-hover:px-2 group-hover:shadow-[0px_0px_10px_0px_rgba(19,30,64,0.05)]'
               : '')
           }
         >
           <p
             ref={textRef}
             className={
-              'text-base font-semibold w-fit line-clamp-2 text-pretty text-text decoration-2 min-w-0 pl-0 leading-5' +
+              'text-base font-normal w-fit line-clamp-1 text-pretty text-text decoration-2 min-w-0 pl-0 leading-5' +
               (isOverflowing ? ' group-hover:line-clamp-none' : '')
             }
           >
