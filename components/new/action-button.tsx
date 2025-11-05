2fb1030e1041477d24d7040272b79b75af34d0f4
Derek Li <derekli2024@gmail.com>
2025-11-05 16:58:40 +0800

    fix: staking errorMessage add currentAccountType

--- components/new/action-button.tsx
+++ components/new/action-button.tsx
@@ -2,19 +2,33 @@
 
 import { useAccount } from 'wagmi';
 
+
+
 import { useEffect, useMemo, useState } from 'react';
+
+
 
 import { ERROR_MESSAGES } from '@/config/const-msg';
 
+
+
 import { WithLoading } from '@/components/common/with-loading';
 import { Button } from '@/components/ui/button';
+
+
 
 import { useSelectedAccount } from '@/lib/data/account-address/use-selected-account';
 import { ErrorVO } from '@/lib/model/error-vo';
 import { useAccountStore } from '@/lib/state/account';
 import { cn } from '@/lib/utils';
 
+
+
 import { ErrorMessage } from './error-message';
+
+
+
+
 
 interface ActionButtonProps {
   disabled: boolean;
@@ -100,7 +114,14 @@ export function ActionButton({
       showError: false,
       errorMessage: '',
     });
-  }, [checkFlag.address, checkFlag.accountInfo, address, accountInfo, isAccountLoading]);
+  }, [
+    checkFlag.address,
+    checkFlag.accountInfo,
+    address,
+    accountInfo,
+    isAccountLoading,
+    currentAccountType,
+  ]);
 
   function handleClick() {
     if (isDisabled || isPending) return;
