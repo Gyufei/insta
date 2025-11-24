bf0381e5b4ea1e9b76e63d6a6dce621d66acf528
Derek Li <derekli2024@gmail.com>
2025-11-24 21:56:24 +0800

    feat: update api

--- lib/data/api-path.ts
+++ lib/data/api-path.ts
@@ -2,7 +2,7 @@ export const isPreview = process.env.NEXT_PUBLIC_IS_PREVIEW === '1';
 // export const isProduction = process.env.NODE_ENV === 'production' && !isPreview;
 export const isProduction = true;
 
-const ProdHost = 'https://api-sandbox.tadle.com';
+const ProdHost = 'https://sb-api.tadle.com';
 const DevHost = 'https://preview-sandbox-api.tadle.com';
 const OddsProdHost = 'https://odds-api.tadle.com';
 const OddsDevHost = 'https://preview-odds-api.tadle.com';
